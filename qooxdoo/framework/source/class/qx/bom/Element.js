e837c22f556309d4d39033b0d8a94a8cfb31f57b
Sebastian Werner <se.werner@telekom.de>
2010-05-04 16:25:13 +0200

    Switched over to toHashCode for window keys in getHelperElement(). Removed whole event support from qx.bom.Element including all dependencies to event handlers/types. This is quite a resource hog and nobody really knows which of them are really used by the application.

--- qooxdoo/framework/source/class/qx/bom/Element.js
+++ qooxdoo/framework/source/class/qx/bom/Element.js
@@ -17,21 +17,8 @@
 
 ************************************************************************ */
 
-/* ************************************************************************
-
-#require(qx.event.dispatch.Direct)
-#require(qx.event.dispatch.DomBubbling)
-#require(qx.event.handler.Keyboard)
-#require(qx.event.handler.Mouse)
-#require(qx.event.handler.DragDrop)
-#require(qx.event.handler.Element)
-#require(qx.event.handler.Appear)
-
-************************************************************************ */
-
 /**
- * This class is mainly a convenience wrapper for DOM elements to
- * qooxdoo's event system.
+ * Basic class for all kind of element modification
  */
 qx.Class.define("qx.bom.Element",
 {
@@ -78,17 +65,16 @@ qx.Class.define("qx.bom.Element",
       }
 
       // key is needed to allow using different windows
-      var key = win.location.href;
+      var key = qx.core.ObjectRegistry.toHashCode(win);
 
       if (!qx.bom.Element.__helperElement[key])
       {
         var helper = qx.bom.Element.__helperElement[key] = win.document.createElement("div");
 
         // innerHTML will only parsed correctly if element is appended to document
-        if (qx.bom.client.Engine.WEBKIT)
+        if (qx.core.Variant.isSet("qx.client", "webkit"))
         {
           helper.style.display = "none";
-
           win.document.body.appendChild(helper);
         }
       }
@@ -184,150 +170,6 @@ qx.Class.define("qx.bom.Element",
      */
     empty : function(element) {
       return element.innerHTML = "";
-    },
-
-
-
-
-
-    /*
-    ---------------------------------------------------------------------------
-      EVENTS
-    ---------------------------------------------------------------------------
-    */
-
-    /**
-     * Add an event listener to a DOM element. The event listener is passed an
-     * instance of {@link Event} containing all relevant information
-     * about the event as parameter.
-     *
-     * @param element {Element} DOM element to attach the event on.
-     * @param type {String} Name of the event e.g. "click", "keydown", ...
-     * @param listener {Function} Event listener function
-     * @param self {Object} Reference to the 'this' variable inside
-     *       the event listener.
-     * @param capture {Boolean} Whether to attach the event to the
-     *       capturing phase of the bubbling phase of the event. The default is
-     *       to attach the event handler to the bubbling phase.
-     * @return {String} An opaque id, which can be used to remove the event listener
-     *       using the {@link #removeListenerById} method.
-     */
-    addListener : function(element, type, listener, self, capture) {
-      return qx.event.Registration.addListener(element, type, listener, self, capture);
-    },
-
-
-    /**
-     * Remove an event listener from a from DOM node.
-     *
-     * Note: All registered event listeners will automatically be removed from
-     *   the DOM at page unload so it is not necessary to detach events yourself.
-     *
-     * @param element {Element} DOM Element
-     * @param type {String} Name of the event
-     * @param listener {Function} The pointer to the event listener
-     * @param self {Object} Reference to the 'this' variable inside
-     *       the event listener.
-     * @param capture {Boolean} Whether to remove the event listener of
-     *       the bubbling or of the capturing phase.
-     */
-    removeListener : function(element, type, listener, self, capture) {
-      return qx.event.Registration.removeListener(element, type, listener, self, capture);
-    },
-
-
-    /**
-     * Removes an event listener from an event target by an id returned by
-     * {@link #addListener}
-     *
-     * @param target {Object} The event target
-     * @param id {String} The id returned by {@link #addListener}
-     */
-    removeListenerById : function(target, id) {
-      return qx.event.Registration.removeListenerById(target, id);
-    },
-
-
-    /**
-     * Check whether there are one or more listeners for an event type
-     * registered at the element.
-     *
-     * @param element {Element} DOM element
-     * @param type {String} The event type
-     * @param capture {Boolean ? false} Whether to check for listeners of
-     *       the bubbling or of the capturing phase.
-     * @return {Boolean} Whether the element has event listeners of the given type.
-     */
-    hasListener : function(element, type, capture) {
-      return qx.event.Registration.hasListener(element, type, capture);
-    },
-
-
-    /**
-     * Focuses the given element. The element needs to have a positive <code>tabIndex</code> value.
-     *
-     * @param element {Element} DOM element to focus
-     * @return {void}
-     */
-    focus : function(element) {
-      qx.event.Registration.getManager(element).getHandler(qx.event.handler.Focus).focus(element);
-    },
-
-
-    /**
-     * Blurs the given element
-     *
-     * @param element {Element} DOM element to blur
-     * @return {void}
-     */
-    blur : function(element) {
-      qx.event.Registration.getManager(element).getHandler(qx.event.handler.Focus).blur(element);
-    },
-
-
-    /**
-     * Activates the given element. The active element receives all key board events.
-     *
-     * @param element {Element} DOM element to focus
-     * @return {void}
-     */
-    activate : function(element) {
-      qx.event.Registration.getManager(element).getHandler(qx.event.handler.Focus).activate(element);
-    },
-
-
-    /**
-     * Deactivates the given element. The active element receives all key board events.
-     *
-     * @param element {Element} DOM element to focus
-     * @return {void}
-     */
-    deactivate : function(element) {
-      qx.event.Registration.getManager(element).getHandler(qx.event.handler.Focus).deactivate(element);
-    },
-
-
-    /**
-     * Captures the given element
-     *
-     * @param element {Element} DOM element to capture
-     * @param containerCapture {Boolean?true} If true all events originating in
-     *   the container are captured. If false events originating in the container
-     *   are not captured.
-     */
-    capture : function(element, containerCapture) {
-      qx.event.Registration.getManager(element).getDispatcher(qx.event.dispatch.MouseCapture).activateCapture(element, containerCapture);
-    },
-
-
-    /**
-     * Releases the given element (from a previous {@link #capture} call)
-     *
-     * @param element {Element} DOM element to release
-     * @return {void}
-     */
-    releaseCapture : function(element) {
-      qx.event.Registration.getManager(element).getDispatcher(qx.event.dispatch.MouseCapture).releaseCapture(element);
     },
 
 
