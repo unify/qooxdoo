9fdca85f1bfc1967a702619d8cbce9828e78bdd6
Sebastian Werner <info@sebastian-werner.net>
2010-06-05 01:24:19 +0200

    Implemented new property type checks based on new qx.core.Type class.

--- qooxdoo/framework/source/class/qx/core/property/Debug.js
+++ qooxdoo/framework/source/class/qx/core/property/Debug.js
@@ -35,45 +35,6 @@ qx.Bootstrap.define("qx.core.property.Debug",
   statics :
   {
     /**
-     * {Map} Built-in checks. The keys could be used in the check of the properties
-     */
-    CHECKS :
-    {
-      "Boolean"   : 'qx.core.Assert.assertBoolean(value, msg) || true',
-      "String"    : 'qx.core.Assert.assertString(value, msg) || true',
-
-      "Number"    : 'qx.core.Assert.assertNumber(value, msg) || true',
-      "Integer"   : 'qx.core.Assert.assertInteger(value, msg) || true',
-      "PositiveNumber" : 'qx.core.Assert.assertPositiveNumber(value, msg) || true',
-      "PositiveInteger" : 'qx.core.Assert.assertPositiveInteger(value, msg) || true',
-
-      "Error"     : 'qx.core.Assert.assertInstance(value, Error, msg) || true',
-      "RegExp"    : 'qx.core.Assert.assertInstance(value, RegExp, msg) || true',
-
-      "Object"    : 'qx.core.Assert.assertObject(value, msg) || true',
-      "Array"     : 'qx.core.Assert.assertArray(value, msg) || true',
-      "Map"       : 'qx.core.Assert.assertMap(value, msg) || true',
-
-      "Function"  : 'qx.core.Assert.assertFunction(value, msg) || true',
-      "Date"      : 'qx.core.Assert.assertInstance(value, Date, msg) || true',
-      "Node"      : 'value !== null && value.nodeType !== undefined',
-      "Element"   : 'value !== null && value.nodeType === 1 && value.attributes',
-      "Document"  : 'value !== null && value.nodeType === 9 && value.documentElement',
-      "Window"    : 'value !== null && value.document',
-      "Event"     : 'value !== null && value.type !== undefined',
-
-      "Class"     : 'value !== null && value.$$type === "Class"',
-      "Mixin"     : 'value !== null && value.$$type === "Mixin"',
-      "Interface" : 'value !== null && value.$$type === "Interface"',
-      "Theme"     : 'value !== null && value.$$type === "Theme"',
-
-      "Color"     : 'qx.lang.Type.isString(value) && qx.util.ColorUtil.isValidPropertyValue(value)',
-      "Decorator" : 'value !== null && qx.theme.manager.Decoration.getInstance().isValidPropertyValue(value)',
-      "Font"      : 'value !== null && qx.theme.manager.Font.getInstance().isDynamic(value)'
-    },
-    
-    
-    /**
      * Validates the incoming parameters of a setter method
      * 
      * @param obj {qx.core.Object} Object which is modified
@@ -82,20 +43,37 @@ qx.Bootstrap.define("qx.core.property.Debug",
      */
     checkSetter : function(obj, config, args)
     {
+      var name = config.name;
+
       if (args.length == 0) {
-        throw new Error("Called set method of property " + config.name + " with no arguments!");
+        throw new Error("Called set() method of property " + name + " with no arguments!");
       }
       
       if (args.length > 1) 
       {
-        obj.warn("Called set method of property " + config.name + " with too many arguments!");
+        obj.warn("Called set() method of property " + name + " with too many arguments!");
         obj.trace();
       }
       
-      // TODO: Type checks
-      //obj.debug("Check: " + config.check); 
-      
-      // Check auf hasOwnProperty vor instanceof Operator wegen memory leak im IE
+      var value = args[0];
+      if (value == null)
+      {
+        if (!config.nullable) {
+          throw new Error("Property " + name + " is not nullable!");
+        }
+      }
+      else
+      {
+        var check = config.check;
+        if (check)
+        {
+          try {
+            qx.core.Type.check(value, check);
+          } catch(ex) {
+            throw new Error("Could not set() property " + name + ": " + ex);
+          }
+        }
+      }
     },
     
 
