f5db91fa785d05523edda31f76f7a1014483ae58
Sebastian Werner <se.werner@telekom.de>
2010-08-26 15:09:03 +0200

    Minor correction

--- sync-demo.sh
+++ sync-demo.sh
@@ -7,4 +7,4 @@ cd ../qooxdoo/qooxdoo/application/demobrowser
 ./generate.py build > /dev/null || exit 1
 cd ~-
 echo ">>> Syncing Demo Browser..."
-rsync -r --delete ../qooxdoo/qooxdoo/application/demobrowser/ demo/
+rsync -r --delete ../qooxdoo/qooxdoo/application/demobrowser/build/* demo/
