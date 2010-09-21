be37c3b1cf09f3773b6c082af684c26ad7b104e2
Sebastian Werner <se.werner@telekom.de>
2010-09-21 14:24:45 +0200

    Added release script

---
+++ release.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+root=`dirname $0`
+echo ">>> Path: $root"
+
+cd $root || exit 1
+version=`cat qooxdoo/version.txt`
+echo ">>> Tagging qooxdoo $version"
+git tag -m "Tagged qooxdoo $version" $version || exit 1
+
+echo ">>> Pushing changes..."
+git push || exit 1
+
+echo ">>> Preparing release..."
+mkdir ../qts-release/qooxdoo-$version || exit 1
+cd ../qts-release/qooxdoo-$version || exit 1
+
+echo ">>> Downloading archive qooxdoo-$version.zip..."
+wget -O qooxdoo-$version.zip http://github.com/unify/qooxdoo/zipball/$version || exit 1
+
+echo ">>> Tagging qooxdoo $version in SVN..."
+cd .. || exit 1
+svn add qooxdoo-$version
+svn ci -m "Tagged qooxdoo $version" || exit 1
+
+echo ">>> Done"
