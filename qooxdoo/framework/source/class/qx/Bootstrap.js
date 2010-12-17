f2777bd2b76b1b70b86e84d1e7a9c1a4ce8d3962
Sebastian Werner <info@sebastian-werner.net>
2010-12-17 23:24:48 +0100

    Fixed ugly dependency issue

--- qooxdoo/framework/source/class/qx/Bootstrap.js
+++ qooxdoo/framework/source/class/qx/Bootstrap.js
@@ -495,14 +495,6 @@ qx.Bootstrap.define("qx.Bootstrap",
     firstUp : function(str) 
     {
       var cache = qx.Bootstrap.$$firstUp;
-      
-      if (qx.core.Variant.isSet("qx.debug", "on"))
-      {
-        if (typeof str != "string") {
-          throw new Error("Invalid type for firstUp(): " + str);
-        }
-      }
-
       var value = cache[str];
       if (value != null) {
         return value;
