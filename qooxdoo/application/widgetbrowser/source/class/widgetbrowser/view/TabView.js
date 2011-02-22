ee02634d36ef4d9263e2f3b412ed0c1e4d9ff0d5
Sebastian Werner <se.werner@telekom.de>
2011-02-22 10:35:17 +0100

    Some cleanups

--- qooxdoo/application/widgetbrowser/source/class/widgetbrowser/view/TabView.js
+++ qooxdoo/application/widgetbrowser/source/class/widgetbrowser/view/TabView.js
@@ -78,12 +78,6 @@ qx.Class.define("widgetbrowser.view.TabView",
       var control = new widgetbrowser.view.TabPage("Control", classname, controls);
       this.add(control);
 
-      // TreeVirtual
-      classname = "widgetbrowser.pages.TreeVirtual";
-      controls = {};
-      var treeVirtual = new widgetbrowser.view.TabPage("TreeVirtual", classname, controls);
-      this.add(treeVirtual);
-
       // Embed
       classname = "widgetbrowser.pages.Embed";
       controls = {};
